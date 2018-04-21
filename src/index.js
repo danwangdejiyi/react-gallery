5dd0228fb567322e7aba536f90408c8f2aeb684c
1476068592 <1476068592@qq.com>
2018-04-21 21:06:01 +0800

    The React Gallery Project was webpack-dev-server,Environmental construction completion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-//This is test
-console.log('webpack react');
-
 ReactDOM.render(
     <div>Hello React</div>,
     document.querySelector('#app')
